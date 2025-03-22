dd500e96f100803f85bf458137c934dd7ab2a567
SagarNewbie234 <sagarbhattacharyamain@gmail.com>
2025-03-22 16:50:36 +0530

    rollback to empty rewrite

--- bin/parze.dart
+++ bin/parze.dart
@@ -1,7 +1,1 @@
-import 'package:parze/parze.dart';
-
-void main(List<String> arguments) {
-  final parser = many(or([letters(), digits()]));
-  final result = parser.run("");
-  print(result);
-}
+void main(List<String> arguments) {}
--- lib/parze.dart
+++ lib/parze.dart
@@ -1,8 +1,1 @@
-library;
 
-export 'parsers/and.dart';
-export 'parsers/string.dart';
-export 'parsers/or.dart';
-export 'parsers/letters.dart';
-export 'parsers/digits.dart';
-export 'parsers/many.dart';
